51ed049d51961ee897f8024b994c56803b3c1002
frazzledfrog <148400964+frazzledfrog@users.noreply.github.com>
2025-07-29 10:52:07 -0400

    Working version before unity update

---
+++ BetaOne/force-app/main/default/lwc/topDealersCompact/topDealersCompact.js
@@ -0,0 +1,54 @@
+import { LightningElement, wire, track } from 'lwc';
+import getTopDealers from '@salesforce/apex/TopDealersController.getTopDealers';
+import getRegions from '@salesforce/apex/TopDealersController.getRegions';
+
+export default class TopDealersCompact extends LightningElement {
+    @track dealers;
+    @track error;
+    @track region = 'Ontario';
+    @track regionOptions = [];
+
+    connectedCallback() {
+        getRegions()
+            .then((result) => {
+                this.regionOptions = result.map(region => ({ label: region, value: region }));
+            })
+            .catch(() => {
+                this.regionOptions = [
+                    { label: 'Ontario', value: 'Ontario' },
+                    { label: 'Alberta', value: 'Alberta' },
+                    { label: 'Quebec', value: 'Quebec' },
+                    { label: 'Atlantic', value: 'Atlantic' },
+                    { label: 'British Columbia', value: 'British Columbia' },
+                    { label: 'Saskatchewan', value: 'Saskatchewan' },
+                    { label: 'Manitoba', value: 'Manitoba' }
+                ];
+            });
+    }
+
+    @wire(getTopDealers, { region: '$region' })
+    wiredDealers({ error, data }) {
+        if (data) {
+            this.dealers = data.map((dealer, idx) => ({
+                ...dealer,
+                rank: idx + 1,
+                Name: dealer.accountName,
+                Metric: dealer.totalAmount,
+                MetricFormatted: this.formatCurrency(dealer.totalAmount)
+            }));
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.dealers = undefined;
+        }
+    }
+
+    handleRegionChange(event) {
+        this.region = event.detail.value;
+    }
+
+    formatCurrency(amount) {
+        if (amount === null || amount === undefined) return '';
+        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: 2 }).format(amount);
+    }
+}
