33ea31ba620b1f7b11d013808965242102fcb2eb
frazzledfrog <148400964+frazzledfrog@users.noreply.github.com>
2025-08-07 10:51:29 -0400

    feat: add simplified quick pdfs using shared styles

---
+++ BetaOne/force-app/main/default/lwc/quickpdfsShared/quickpdfsShared.js
@@ -0,0 +1,50 @@
+// QuickpdfsShared component uses shared style colors for a simple PDF list
+import { LightningElement } from 'lwc';
+import { loadStyle } from 'lightning/platformResourceLoader';
+import sharedStyles from '@salesforce/resourceUrl/sharedStyles';
+import VIN_Amendment from '@salesforce/resourceUrl/VIN_Amendment';
+import Trustee_Form from '@salesforce/resourceUrl/Trustee_Form';
+import RecSheetPDF from '@salesforce/resourceUrl/RecSheetPDF';
+import Dealer_Sign_Up_Package_ENG from '@salesforce/resourceUrl/Dealer_Sign_Up_Package_ENG';
+import Dealer_Sign_Up_Package_FR from '@salesforce/resourceUrl/Dealer_Sign_Up_Package_FR';
+import Proof_of_Insurance_Form from '@salesforce/resourceUrl/Proof_of_Insurance_Form';
+
+export default class QuickpdfsShared extends LightningElement {
+    pdfFiles = [
+        { id: 'vin_amendment', name: 'VIN Amendment', resourceUrl: VIN_Amendment },
+        { id: 'trustee_form', name: 'Trustee Form', resourceUrl: Trustee_Form },
+        { id: 'rec_sheet_pdf', name: 'Recreational Vehicle Finance Sheet', resourceUrl: RecSheetPDF },
+        { id: 'dealer_signup_eng', name: 'Dealer Sign Up Package (English)', resourceUrl: Dealer_Sign_Up_Package_ENG },
+        { id: 'dealer_signup_fr', name: 'Dealer Sign Up Package (French)', resourceUrl: Dealer_Sign_Up_Package_FR },
+        { id: 'proof_of_insurance', name: 'Proof of Insurance Form', resourceUrl: Proof_of_Insurance_Form }
+    ];
+
+    get hasPdfFiles() {
+        return this.pdfFiles && this.pdfFiles.length > 0;
+    }
+
+    connectedCallback() {
+        loadStyle(this, sharedStyles);
+    }
+
+    handlePreview(event) {
+        const fileId = event.target.dataset.fileId;
+        const file = this.pdfFiles.find((f) => f.id === fileId);
+        if (file && file.resourceUrl) {
+            window.open(file.resourceUrl, '_blank');
+        }
+    }
+
+    handleDownload(event) {
+        const fileId = event.target.dataset.fileId;
+        const file = this.pdfFiles.find((f) => f.id === fileId);
+        if (file && file.resourceUrl) {
+            const link = document.createElement('a');
+            link.href = file.resourceUrl;
+            link.download = file.name + '.pdf';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+}
